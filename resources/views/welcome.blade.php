78469f65931e860271aa6a8742773c9b167578ec
Jordan Hoff <jhoff@linkedin.com>
2018-01-15 15:01:33 -0800

    Update links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,9 +83,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://www.meetup.com/Laravel-Omaha">Meetup</a>
-                    <a href="https://www.youtube.com/channel/UC5y0QfbLdP0Ytvst4uMd-2w">Videos</a>
-                    <a href="https://github.com/laravel-omaha/laravel-omaha">Contribute</a>
+                    <a target="_blank" href="https://www.meetup.com/Laravel-Omaha">Meetup</a>
+                    <a target="_blank" href="https://www.youtube.com/channel/UC5y0QfbLdP0Ytvst4uMd-2w">YouTube</a>
+                    <a target="_blank" href="https://twitter.com/laravelomaha">Twitter</a>
+                    <a target="_blank" href="https://github.com/laravel-omaha/laravel-omaha">Contribute</a>
                 </div>
             </div>
         </div>
